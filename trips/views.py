77b62b5f0b389046b6c24ad6dd0edb524b473245
Tom Ford <tom@tom-5m-lt0.gha.chartermi.net>
2021-05-02 12:23:55 -0400

    first 3 parts

---
+++ trips/views.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions, viewsets
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import UserSerializer, LogInSerializer, TripSerializer
+from .models import Trip
+
+
+class TripView(viewsets.ReadOnlyModelViewSet):
+    lookup_field = 'id'
+    lookup_url_kwarg = 'trip_id'
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+
+
+class SignUpView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class LogInView(TokenObtainPairView):
+    serializer_class = LogInSerializer
